b407acb18137b8565ee5e124febafc699252b8f3
Raul Christian <113435873+raulss179@users.noreply.github.com>
2023-06-27 13:47:29 -0300

    criando formulario de contato dinamico entrada para banco de dados console log para verificar se os dados estão sendo guardados proximos passos: adicionar verificação se todos os campos foram preenchidos e condição de ddd e 9 numeros para o whatsApp

--- desafio-01/src/Menu.js
+++ desafio-01/src/Menu.js
@@ -1,6 +1,10 @@
 import React , {useState} from 'react';
 import './Menu.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdMiscellaneousServices } from 'react-icons/md';
+import { BsPersonCheckFill } from 'react-icons/bs';
+import { GiPriceTag } from 'react-icons/gi';
+import { MdContactPhone } from 'react-icons/md';
 
 function Menu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,10 +22,10 @@ function Menu() {
       {isMenuOpen && (
         <nav className='menu_options'>
           <ul>
-            <li><a href="#secao01">Soluções</a></li>
-            <li><a href="#secao02">Clientes</a></li>
-            <li><a href="#secao03">Preços</a></li>
-            <li><a href="#secao04">Contato</a></li>
+            <li><a href="#secao01"><MdMiscellaneousServices/> Soluções</a></li>
+            <li><a href="#secao02"><BsPersonCheckFill/> Clientes</a></li>
+            <li><a href="#secao03"><GiPriceTag/> Preços</a></li>
+            <li><a href="#secao04"><MdContactPhone/> Contato</a></li>
           </ul>
         </nav>
       )}
